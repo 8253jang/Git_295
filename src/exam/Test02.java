26e231ce21ea2e2d3abb083582f9b9c55eba4b9c
BongBong <8253jang@daum.net>
2025-03-10 23:19:15 +0900

    stash pop 후 충돌 해결 커밋

--- src/exam/Test02.java
+++ src/exam/Test02.java
@@ -6,7 +6,11 @@ public class Test02 {
 	  System.out.println("원격에서 두번째 수정했음");
 	  System.out.println("로컬에서 같은 부분 수정중....");
 	  System.out.println("원격에서 세번째 수정함");
-           System.out.println("원격에서 네번째 수정함");
+
+      System.out.println("원격에서 네번째 수정함");
+
+	  System.out.println("로컬에서 네번째 수정함");
+
   }
 
 }
